f3b6a2ee6c2fecfb6715f7366f985dfb0bcb03aa
Eric Haberstroh <gpg@erixpage.de>
2021-12-01 11:23:40 +0100

    Initialize input and puzzle class

---
+++ src/Puzzle/Day01Puzzle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace PilleAoc2021\Puzzle;
+
+final class Day01Puzzle extends AbstractPuzzle implements PuzzleInterface
+{
+    public static string $puzzleName = 'day01';
+
+    public function solve(): string
+    {
+        // Access input via $this->input
+    }
+}
