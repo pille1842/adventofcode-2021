2a7951429aabf5968c5f6aee43d74ff0425439a7
Eric Haberstroh <gpg@erixpage.de>
2021-12-06 14:38:45 +0100

    Make day 6 even more concise

--- src/Puzzle/Day06Puzzle.php
+++ src/Puzzle/Day06Puzzle.php
@@ -16,19 +16,19 @@ final class Day06Puzzle extends AbstractPuzzle implements PuzzleInterface
 
         $solutionPart1 = 0;
 
-        for ($gen = 0; $gen < 256; $gen++) {
-            $fish['spawn'] = $fish[0];
+        for ($gen = 1; $gen <= 256; $gen++) {
+            $spawn = $fish[0];
             for ($i = 0; $i < 8; $i++) {
                 $fish[$i] = $fish[$i + 1];
             }
-            $fish[6] += $fish['spawn'];
-            $fish[8] = $fish['spawn'];
-            if ($gen == 79) {
-                $solutionPart1 = array_sum($fish) - $fish['spawn'];
+            $fish[6] += $spawn;
+            $fish[8] = $spawn;
+            if ($gen == 80) {
+                $solutionPart1 = array_sum($fish);
             }
         }
 
-        $solutionPart2 = array_sum($fish) - $fish['spawn'];
+        $solutionPart2 = array_sum($fish);
 
         return sprintf(
             "# of fish after  80 days: %d\n# of fish after 256 days: %d",
